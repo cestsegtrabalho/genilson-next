91eb6a6fd1fe3eef7dee1d4e5aabbc10baa4f1aa
Stanley Andrade <stanleyandrade95@gmail.com>
2026-02-17 13:30:56 -0300

    update

--- app/api/user/login/route.ts
+++ app/api/user/login/route.ts
@@ -82,8 +82,8 @@ export async function OPTIONS() {
   return new Response(null, {
     status: 204,
     headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
   });
