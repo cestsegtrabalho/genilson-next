cd5c6f55e234dc39e6b415f4783addbbedd89704
Stanley Andrade <stanleyandrade95@gmail.com>
2026-02-17 13:19:30 -0300

    cors

--- app/middlewares/authenticateToken.ts
+++ app/middlewares/authenticateToken.ts
@@ -4,9 +4,20 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
 
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  response.headers.set(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3000"
+  );
+
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
 
   return response;
 }
